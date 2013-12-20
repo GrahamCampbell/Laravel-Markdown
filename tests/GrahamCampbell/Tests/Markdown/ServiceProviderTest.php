8d08443a05f287894ebb4a4caa5bae4c00ef39f4
Graham Campbell <graham@mineuk.com>
2013-12-20 21:55:48 +0000

    Added tests

---
+++ tests/GrahamCampbell/Tests/Markdown/ServiceProviderTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of Laravel Markdown by Graham Campbell.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace GrahamCampbell\Tests\Markdown;
+
+use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
+
+/**
+ * This is the service provider test class.
+ *
+ * @package    Laravel-Markdown
+ * @author     Graham Campbell
+ * @copyright  Copyright 2013 Graham Campbell
+ * @license    https://github.com/GrahamCampbell/Laravel-Markdown/blob/develop/LICENSE.md
+ * @link       https://github.com/GrahamCampbell/Laravel-Markdown
+ */
+abstract class ServiceProviderTest extends AbstractTestCase
+{
+    use ServiceProviderTestCaseTrait;
+
+    /**
+     * Get the service provider class.
+     *
+     * @return string
+     */
+    protected function getServiceProviderClass()
+    {
+        return 'GrahamCampbell\Markdown\MarkdownServiceProvider';
+    }
+}

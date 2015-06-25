61c7e13e8a67225f8e22a6f2ade5ac5535278fe3
Graham Campbell <graham@alt-three.com>
2015-06-25 14:38:23 +0100

    Updated email

--- src/Engines/BladeMarkdownEngine.php
+++ src/Engines/BladeMarkdownEngine.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Markdown.
  *
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Graham Campbell <graham@alt-three.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -18,7 +18,7 @@ use League\CommonMark\CommonMarkConverter;
 /**
  * This is the php markdown engine class.
  *
- * @author Graham Campbell <graham@cachethq.io>
+ * @author Graham Campbell <graham@alt-three.com>
  */
 class BladeMarkdownEngine extends CompilerEngine
 {
--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Markdown.
  *
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Graham Campbell <graham@alt-three.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@ use GrahamCampbell\TestBench\AbstractPackageTestCase;
 /**
  * This is the abstract test case class.
  *
- * @author Graham Campbell <graham@cachethq.io>
+ * @author Graham Campbell <graham@alt-three.com>
  */
 abstract class AbstractTestCase extends AbstractPackageTestCase
 {

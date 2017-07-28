83d717a7b2a6e487646be8303b1539f27bffd01e
Graham Campbell <graham@alt-three.com>
2017-07-28 14:26:07 +0100

    Added analyzer to tests

---
+++ tests/AnalysisTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Laravel Markdown.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GrahamCampbell\Tests\Markdown;
+
+use GrahamCampbell\Analyzer\AnalysisTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This is the analysis test class.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+class AnalysisTest extends TestCase
+{
+    use AnalysisTrait;
+
+    protected function getPaths()
+    {
+        return [
+            realpath(__DIR__.'/../config'),
+            realpath(__DIR__.'/../src'),
+            realpath(__DIR__),
+        ];
+    }
+}

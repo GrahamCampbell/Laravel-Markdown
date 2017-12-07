453a9ba1657b659ca65d47dc688053179383b080
MegaXLR <tedvanriel@gmail.com>
2017-12-07 11:41:13 +0100

    Add Htmlable support
    Fixes GrahamCampbell/Laravel-Markdown#91
    
    Signed-off-by: MegaXLR <admin@megaxlr.net>

---
+++ src/Converters/Converter.php
@@ -0,0 +1,23 @@
+<?php
+namespace GrahamCampbell\Markdown\Converters;
+
+use GrahamCampbell\Markdown\Responses\MarkdownString;
+
+class Converter extends \League\CommonMark\Converter {
+	
+	/**
+	 * Converts CommonMark to HTML.
+	 *
+	 * @param string $commonMark
+	 *
+	 * @return string
+	 *
+	 * @api
+	 */
+	public function convertToHtml($commonMark)
+	{
+		$documentAST = $this->docParser->parse($commonMark);
+		
+		return new MarkdownString($this->htmlRenderer->renderBlock($documentAST));
+	}
+}

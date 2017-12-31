98e94664d436fb2279e19b6042d97a1371f8bb50
Graham Campbell <graham@alt-three.com>
2017-12-31 17:30:35 +0000

    Fixed config in phars

--- src/MarkdownServiceProvider.php
+++ src/MarkdownServiceProvider.php
@@ -56,7 +56,7 @@ class MarkdownServiceProvider extends ServiceProvider
      */
     protected function setupConfig()
     {
-        $source = realpath(__DIR__.'/../config/markdown.php');
+        $source = realpath($raw = __DIR__.'/../config/markdown.php') ?: $raw;
 
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('markdown.php')]);

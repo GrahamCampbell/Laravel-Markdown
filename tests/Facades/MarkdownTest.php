36d7328cd323e0abeb365fb4d0aa08ccf36e4bab
Graham Campbell <graham@alt-three.com>
2020-04-14 15:06:07 +0100

    Fixed tests

--- tests/Facades/MarkdownTest.php
+++ tests/Facades/MarkdownTest.php
@@ -17,7 +17,7 @@ use GrahamCampbell\Markdown\Facades\Markdown;
 use GrahamCampbell\TestBenchCore\FacadeTrait;
 use GrahamCampbell\Tests\Markdown\AbstractTestCase;
 use League\CommonMark\Converter;
-use League\CommonMark\Ext\SmartPunct\SmartPunctExtension;
+use League\CommonMark\Extension\SmartPunct\SmartPunctExtension;
 
 /**
  * This is the markdown facade test class.
